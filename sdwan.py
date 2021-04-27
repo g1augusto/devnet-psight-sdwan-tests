7288a22a54a8c7c31572439915a194412c196f62
g1augusto <giovanni@G1-Laptop.odin.local>
2021-04-27 21:50:31 +0200

    Linted code and added gitignore

--- sdwan.py
+++ sdwan.py
@@ -1,20 +1,27 @@
+'''
+This program will test SDWAN connection and REST API function to
+SDWAN Devnet SANDBOX (no Python SDK)
+'''
 import requests
+import urllib3
 
-# requests.session creates an object that has all the features of a traditional "requests" object but retain information
+# requests.session creates an object that has all the features of a
+# traditional "requests" object but retain information
 # about the TCP connection and HTTP parameters (like authentication and COOKIES)
 
 def sdwanlogin(session,inputdata):
-
-    # https://developer.cisco.com/docs/sdwan/#!authentication/how-to-authenticate
-    # Authentication to Cisco SDWAN (Viptela) is against the vManage orchestrator with a series of steps
-    # 1) pass the authentication credentials in the body and retrive JSESSIONID Cookie
-    #    credentials can be passed as a dictionary of two elements or a single element (commented below)
-    # 2) Request a Cross site forgery request token (XSRF Token) that is required for POST operations with the JSESSIONID Cookie
-    #    reusing the same session (carrying JESSIONID cookie) we can request an XSRF token that will be returned in the BODY
-    #    of the response (if successful)
-    # 3) Once Cookie is retained and the XSRF token is obtained, is possible to make requests to the vManage via REST API for both
-    #    POST and GET operations
-
+    '''
+    https://developer.cisco.com/docs/sdwan/#!authentication/how-to-authenticate
+    Authentication to Cisco SDWAN (Viptela) is against the vManage orchestrator
+    with a series of steps
+    1) pass the authentication credentials in the body and retrive JSESSIONID Cookie credentials
+       can be passed as a dictionary of two elements or a single element (commented below)
+    2) Request a Cross site forgery request token (XSRF Token) that is required for POST operations
+       with the JSESSIONID Cookie reusing the same session (carrying JESSIONID cookie) we can
+       request an XSRF token that will be returned in the BODY of the response (if successful)
+    3) Once Cookie is retained and the XSRF token is obtained, is possible to make requests
+       to the vManage via REST API for both POST and GET operations
+    '''
     # Step 1 --- Authentication
     response = session.post(f"{inputdata['baseurl']}{inputdata['securitycheckurl']}",
             headers={"Content-Type":"application/x-www-form-urlencoded"},
@@ -23,8 +30,10 @@ def sdwanlogin(session,inputdata):
     return response.text
 
 def sdwantoken(session,inputdata):
-
-    # Step 2 --- Retrieve XSRF token and update the session headers to include it in future requests (cookie JESSIONID is retained)
+    '''
+    Step 2 --- Retrieve XSRF token and update the session headers to include it in future
+               requests (cookie JESSIONID is retained)
+    '''
     response = session.get(f"{inputdata['baseurl']}{inputdata['tokenurl']}",
             headers={"Content-Type":"application/json"},
             verify=False)
@@ -33,11 +42,12 @@ def sdwantoken(session,inputdata):
     return xsrftoken
 
 def sdwanlistdevices(session,inputdata):
-
-    # Step 3 --- Execute requests (retrieve device list from vManage)
+    '''
+    Step 3 --- Execute requests (retrieve device list from vManage)
+    '''
     try:
         response = session.get(f"{inputdata['baseurl']}{inputdata['devicelisturl']}",verify=False)
-    except:
+    except Exception:
         return "<html>"
 
     # Retrieve device list and print certain data for each device
@@ -49,7 +59,7 @@ def sdwanlistdevices(session,inputdata):
 
 if __name__ == "__main__":
     # Disable requests security warnings for SSL certificate
-    requests.packages.urllib3.disable_warnings()
+    urllib3.disable_warnings()
 
     data = {"baseurl":"https://sandbox-sdwan-1.cisco.com",
             "securitycheckurl" : "/j_security_check",
@@ -63,5 +73,4 @@ if __name__ == "__main__":
     sdwanlistdevices(mysession,data)
 else:
     # Disable requests security warnings for SSL certificate
-    requests.packages.urllib3.disable_warnings()
-
+    urllib3.disable_warnings()
